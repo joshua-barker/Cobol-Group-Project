4df92911bb5dbf1aec4e7dc7549fcd4a066228ba
caleb005 <stevensc005@mymail.mssu.edu>
2017-04-17 20:45:07 -0500

    Update MENU.cbl

--- MENU.cbl
+++ MENU.cbl
@@ -28,26 +28,22 @@
                DISPLAY CLEAR-SCREEN
                DISPLAY MENU-SCREEN
                ACCEPT MENU-SCREEN
-
                IF OPTION = 'A' OR 'F' OR 'R' OR 'E'
                    DISPLAY SNO-SCREEN
                    ACCEPT SNO-SCREEN
-               END-IF
-               DISPLAY CLEAR-SCREEN
-
-               IF OPTION = 'A'
-                   CALL 'PROJ-ADD-STUDENT' USING WS-SNO
-               ELSE IF OPTION = 'C'
-                   CALL '(ADD-CLASS-PGM)'
-               ELSE IF OPTION = 'F'
-                   CALL '(FINANCIAL-AID-PGM)' USING WS-SNO
-               ELSE IF OPTION = 'R'
-                   CALL '(ADD-RECEIPTS-PGM)' USING WS-SNO
-               ELSE IF OPTION = 'E'
-                   CALL 'PROJ-ENROLLMENT' USING WS-SNO
-               ELSE
-                   CALL '(PRINT-REPORT-PGM)'
-
+                   IF OPTION = 'A'
+                       CALL 'PROJ-ADD-STUDENT' USING WS-SNO
+                   ELSE IF OPTION = 'C'
+                       CALL '(ADD-CLASS-PGM)'
+                   ELSE IF OPTION = 'F'
+                       CALL '(FINANCIAL-AID-PGM)' USING WS-SNO
+                   ELSE IF OPTION = 'R'
+                       CALL '(ADD-RECEIPTS-PGM)' USING WS-SNO
+                   ELSE IF OPTION = 'E'
+                       CALL 'PROJ-ENROLLMENT' USING WS-SNO
+                   ELSE
+                       CALL '(PRINT-REPORT-PGM)'
+                   END-IF
                END-IF
                DISPLAY CLEAR-SCREEN
            END-PERFORM
